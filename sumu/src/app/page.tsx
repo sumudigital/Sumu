2840bdc8516fc35243fcf8d17714d8b53cb103c1
Thomas Musengwa <thomas.musengwa@accella.net>
2023-08-16 03:08:56 -0500

    :tada: added next.js and created homepage

---
+++ sumu/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center  p-24">
+      <h1>Hello World!</h1>
+      <p>Welcome to Semu!</p>
+    </main>
+  );
+}
